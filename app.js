b8bef65a6446969f9b70934226510664d4389a9e
wonee1 <gimchaewon417@gmail.com>
2026-05-22 16:59:38 +0900

    init : index, app 분리

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const aiRecommendRoutes = require('./routes/aiRecommendRoutes');
+const timeRecommendations = require('./routes/timeRecommendations');
+const recommendations = require('./routes/recommendations');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({ origin: process.env.GATEWAY_URL, credentials: true }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api/recommend', aiRecommendRoutes); 
+app.use('/api/time-recommendations', timeRecommendations);
+app.use('/api/recommendations', recommendations);
+
+module.exports = app;
