47e94fe8b3b96eca4b5ba2d8c03041a59b7e5aca
wonee1 <gimchaewon417@gmail.com>
2026-05-28 13:48:12 +0900

    feat : challenge, user 호출 코드

---
+++ clients/userClient.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const USER_SVC = process.env.USER_SERVICE_URL;
+
+// GET /internal/preferences/user/:userId
+// 응답 예시: { age, interests: [{ interest_id }], visions: [{ vision_id }] }
+exports.getPreferences = async (userId) => {
+  const { data } = await axios.get(`${USER_SVC}/internal/preferences/user/${userId}`);
+  return data;
+};
