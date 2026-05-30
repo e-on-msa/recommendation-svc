03dd5f4252d28baaa5b35ba144043702c467b156
wonee1 <gimchaewon417@gmail.com>
2026-05-30 16:00:34 +0900

    feat : challenge url 수정

--- clients/challengeClient.js
+++ clients/challengeClient.js
@@ -2,17 +2,8 @@ const axios = require('axios');
 
 const CHALLENGE_SVC = process.env.CHALLENGE_SERVICE_URL;
 
-// GET /internal/challenges/with-stats
-// 응답 예시: [{
-//   challenge_id, challenge_title, challenge_description,
-//   minimum_age, maximum_age, challenge_state,
-//   start_date, end_date, application_deadline,
-//   is_recuming, intermediate_participation,
-//   participant_count, avg_rating,
-//   interest_ids: [1, 2],
-//   vision_ids: [3, 4],
-// }]
+// GET /internal/challenges/active-with-categories
 exports.getChallengesWithStats = async () => {
-  const { data } = await axios.get(`${CHALLENGE_SVC}/internal/challenges/with-stats`);
-  return data;
+  const { data } = await axios.get(`${CHALLENGE_SVC}/internal/challenges/active-with-categories`);
+  return data.challenges || [];
 };
